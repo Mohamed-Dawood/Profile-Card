85718ae9f288445fb68b9f1423efb136ad234972
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2024-10-23 01:24:06 +0300

    Profile Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Image from './images/Mohamed Dawood.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList />
+        </div>
+      </div>
     </div>
   );
 }
 
+function Avatar() {
+  return <img className="avatar" src={Image} alt="mohamed dawood" />;
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Mohamed Dawood</h1>
+      <p>
+        Passionate software engineer driven by a love for coding, focused on
+        growth, success, and innovation. Always learning and adapting to excel.
+      </p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="JavaScript" emoji="👨‍🦳" color="yellow" />
+      <Skill skill="C++" emoji="💪" color="red" />
+      <Skill skill="Html&Css" emoji="👨‍🦰" color="orange" />
+      <Skill skill="Node.js" emoji="💪" color="green" />
+      <Skill skill="Svelte" emoji="👶" color="gray" />
+    </div>
+  );
+}
+
+function Skill({ skill, emoji, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>{emoji}</span>
+    </div>
+  );
+}
 export default App;
